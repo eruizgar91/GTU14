73429f6ca24afb8ff1e40a886813dae83b85a9ab
Gonzalo Pérez-Tomé Estévez <rmva2@hotmail.com>
2014-03-27 12:57:22 +0100

    Creado proyecto inicial de netbeans.

---
+++ GTU14/src/java/com/gtu14/database/Dao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gtu14.database;
+
+import javax.ejb.Singleton;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author gonzalo
+ */
+@Singleton
+@ApplicationScoped
+public class Dao {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
