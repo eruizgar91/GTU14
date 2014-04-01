9c41d578a16dcde9372912d7861d3aa43a5dc057
Gonzalo Pérez-Tomé Estévez <gonzalo.g88@gmail.com>
2014-04-01 08:28:12 -0700

    Creado proyecto GTU14 en Eclipse

---
+++ GTU14/src/com/gtu14/persistence/Dao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gtu14.persistence;
+
+import javax.ejb.Singleton;
+import javax.enterprise.context.ApplicationScoped;
+
+@Singleton
+@ApplicationScoped
+public class Dao {
+	
+}
